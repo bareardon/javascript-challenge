1a12623aa92d903e2348acee6b98214cc1d73029
bareardon <73446403+bareardon@users.noreply.github.com>
2021-02-26 17:27:16 -0500

    set up of part 2

--- UFO-level-2/StarterCode/static/js/app.js
+++ UFO-level-2/StarterCode/static/js/app.js
@@ -1,4 +1,58 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// from data.js
+var tableData = data;
+
+// Set up a reference to the table body
+var tbody = d3.select("tbody");
+
+// Create function to display UFO sightings
+function tableDisplay(ufoSightings) {
+    tbody.html("");
+    ufoSightings.forEach((ufoRecords) => {
+        var row = tbody.append("tr");
+        Object.entries(ufoRecords).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
+
+// Select the filter-table button
+var button = d3.select("#filter-btn");
+
+// Create an event that triggers button when clicked 
+function controlClick() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element, get the raw HTML node and get the value property of the input element
+    var date = d3.select("#datetime").property("value");
+
+    // console.log(date);
+    var filterData = tableData;
+
+    // Filter through date/time column to return results that match user input 
+    if(date === "") {
+        // Show whole table if no date is entered
+        
+        // Build table with data
+        tableDisplay(filterData);
+        } 
+    else {
+    // Apply filter, collect data to keep rows that return criteria
+        filterData = filterData.filter((ufoSightings) => ufoSightings.datetime === date);
+        console.log(filterData);
+    }
+        // Build table with data
+        tableDisplay(filterData);
+};
+
+// Attach an event to the click function 
+d3.selectAll("#filter-btn").on("click", controlClick);
+
+// Display UFO sightings
+// console.log(tableData);
+tableDisplay(tableData);
